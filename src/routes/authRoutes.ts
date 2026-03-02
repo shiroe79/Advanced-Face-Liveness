62473ab53b820bdb140efee33b4adf10579f2d8c
Biny <login.new.world@gmail.com>
2026-03-02 23:32:46 +0300

    routes for auth flow (basic)

---
+++ src/routes/authRoutes.ts
@@ -0,0 +1,21 @@
+import { Router } from 'express'
+
+const router = Router()
+
+
+// Auth routes
+router.post('/register' , (req, res) => {
+    res.status(201).json({message: 'User registered'})
+})
+
+router.post('/login', (req, res) => {
+    res.status(201).json({message: 'User logged in'})
+})
+
+router.post('/logout', (req, res) => {
+    res.status(201).json({message: 'User logged out'})
+})
+
+router.post('/refresh', (req, res) => {
+    res.status(201).json({message: 'Token refreshed'})
+})
